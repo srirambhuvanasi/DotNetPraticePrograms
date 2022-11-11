2816ff3b6c4c70b57dd372ff30fc911da6b26922
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-11 20:48:00 +0530

    sum of number in a string

---
+++ sum of number in a string/sum of number in a string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_number_in_a_string
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = "hhf5454dg66974dhd6548hgd888";
+			
+			int sum = 0;
+			for(int i=0;i<s.Length;i++)
+			{
+				if((s[i] >= '0') && (s[i] <= '9'))
+				{
+					sum += (s[i] - '0');
+				}
+			}
+			Console.WriteLine("Sum = " + sum);
+			Console.ReadLine();
+		}
+	}
+}
