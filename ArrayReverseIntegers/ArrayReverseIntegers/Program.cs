b39df5cb0872dd740715a5335c8c0efe26292ec4
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-11 13:44:43 +0530

    Array integers

---
+++ ArrayReverseIntegers/ArrayReverseIntegers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayReverseIntegers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = { 1, 2, 3, 4, 5, 6 };
+			Array.Reverse(arr);
+			foreach(int value in arr)
+			{
+				Console.Write(value);
+			}
+			Console.ReadLine();
+		}
+	}
+}
