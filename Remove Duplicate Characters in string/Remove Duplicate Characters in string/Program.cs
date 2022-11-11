fd9c21d449e852c7ed3555c3d6a43f5af0ae5619
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-11 14:12:52 +0530

    Remove duplicate charcters

---
+++ Remove Duplicate Characters in string/Remove Duplicate Characters in string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Duplicate_Characters_in_string
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s1 = "Google";
+			string s2 = " ";
+			string s3 = " ";
+			foreach(char value in s1)
+			{
+				if(s2.IndexOf(value)==-1)
+				{
+					s2 += value;
+					s3 += value;
+				}
+			}
+			Console.WriteLine(s3);
+			Console.ReadLine();
+		}
+	}
+}
