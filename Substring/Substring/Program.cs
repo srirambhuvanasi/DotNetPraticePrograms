12a4707463c1537d423097fdd0ece359d6ed4e16
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-11 19:21:29 +0530

    Find all substring

---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string value = "abc";
+			// Avoid full length.
+			for (int i = 1; i < value.Length; i++)
+			{
+				// End index is tricky.
+				for (int j = 0; j <= value.Length - i; j++)
+				{
+					string substring = value.Substring(j,i);
+					Console.WriteLine(substring);
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
