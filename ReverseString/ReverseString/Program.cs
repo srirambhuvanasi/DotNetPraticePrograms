6cb34ac70865f960a9b6e4362abc359523fb25a7
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-11 13:38:13 +0530

    Reverse string PGM added

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a string");
+			string s1 = Console.ReadLine();
+			string s2 = " ";
+			for(int i=s1.Length-1;i>=0;i--)
+			{
+				s2 = s2 + s1[i];
+			}
+			Console.WriteLine(s2);
+			Console.ReadLine();
+		}
+	}
+}
