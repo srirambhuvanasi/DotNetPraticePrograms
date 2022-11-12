c24e022c644770b8fac8a046bfc701df2181ace0
srirambhuvanasi <srirambhuvanasi@gmail.com>
2022-11-12 08:55:43 +0530

    Remove Duplicatewords and numbers ina array using

---
+++ Remove duplicate words in string/Remove duplicate words in string/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_duplicate_words_in_string
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = { 1, 2, 3, 4, 3, 55, 23, 2 };
+			int[] dist = nums.Distinct().ToArray();
+
+			Array.ForEach(dist, v => Console.WriteLine(v));
+
+			string[] animals = { "Cat", "Alligator", "Fox", "Donkey", "Cat", "Fox" };
+
+			string[] d = animals.Distinct().ToArray();
+
+			Array.ForEach(d, v => Console.WriteLine(v));                           
+			Console.ReadLine();
+		}
+	}
+}
