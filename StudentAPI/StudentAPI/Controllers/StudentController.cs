02bb9812fc47b9d24d25710327be16bb2fa94d65
srirambhuvanasi <srirambhuvanasi@gmail.com>
2023-11-13 12:55:32 +0530

    WEB API Curd Operations

---
+++ StudentAPI/StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentAPI.Models;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly APIDBContext _context;
+
+        public StudentController(APIDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Student
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> Getstudents()
+        {
+          if (_context.students == null)
+          {
+              return NotFound();
+          }
+            return await _context.students.ToListAsync();
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+          if (_context.students == null)
+          {
+              return NotFound();
+          }
+            var student = await _context.students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Student/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Student
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+          if (_context.students == null)
+          {
+              return Problem("Entity set 'APIDBContext.students'  is null.");
+          }
+            _context.students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.StudentId }, student);
+        }
+
+        // DELETE: api/Student/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            if (_context.students == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();//204 
+        }
+
+        private bool StudentExists(int id)
+        {
+            return (_context.students?.Any(e => e.StudentId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ StudentAPI/StudentAPI/Models/APIDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAPI.Models
+{
+    public class APIDBContext:DbContext
+    {
+        public APIDBContext(DbContextOptions options):base (options)
+        {
+
+        }
+        public DbSet<Student>students { get; set; }
+    }
+}
