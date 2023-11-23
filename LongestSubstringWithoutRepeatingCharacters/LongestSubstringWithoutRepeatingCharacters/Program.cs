89ce0025a9e7a7fb55f40d64c3646cc7d45b11b0
srirambhuvanasi <srirambhuvanasi@gmail.com>
2023-11-23 22:41:47 +0530

    Logical PGMS

---
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    internal class Program
+    {
+        public static int LongestSubstring(string s)
+        {
+            int maxLength = 0;
+            string maxChar = null;
+            int start = 0;
+            Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charIndexMap.ContainsKey(s[i]))
+                {
+                    start = Math.Max(start, charIndexMap[s[i]] + 1);
+                }
+
+                charIndexMap[s[i]] = i;
+                maxLength = Math.Max(maxLength, i - start + 1);
+                
+            }
+            
+            return maxLength;
+        }
+        static void Main(string[] args)
+        {
+            string s = "GREEKSFORGREEKS";
+            int result=LongestSubstring(s);
+            Console.WriteLine($"Length of the longest substring without repeating characters: {result}");
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ StringCompressionoccurrences/StringCompressionoccurrences/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCompressionoccurrences
+{
+    internal class Program
+    {
+        public static void StringCompression(string s)
+        {
+            char temp = s[0];
+            int count = 1;
+            for (int i = 1; i < s.Length; i++)
+            {  
+                    if (s[i] == temp)
+                    {
+                        count++;                      
+                    }
+                    else
+                    {
+                    Console.Write(temp.ToString() + count);
+                    temp = s[i];
+                    count = 1;
+                    }
+            }
+            Console.Write(temp.ToString());
+            Console.Write(count);
+        }
+
+        static void Main(string[] args)
+        {
+            string s1 = "aaabbbffgg";
+           
+            StringCompression(s1);
+          
+            Console.ReadLine();
+        }
+
+       
+    }
+}
